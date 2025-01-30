ff45eadf1bb70c65179af58aba5f361ee8dca25b
Martin <gagochorenmartin@gmail.com>
2025-01-30 14:03:29 +0100

    +function to insert examples related with definitions

--- API/src/main/java/com/martingago/words/service/batchInsertion/BatchInsertionDefinitionService.java
+++ API/src/main/java/com/martingago/words/service/batchInsertion/BatchInsertionDefinitionService.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Slf4j
 @Service
@@ -22,6 +19,9 @@ public class BatchInsertionDefinitionService {
 
     @Autowired
     WordDefinitionRepository wordDefinitionRepository;
+
+    @Autowired
+    BatchesInsertionExamplesService batchesInsertionExamplesService;
 
     @Transactional
     public Map<String, DefinitionEstructurePojo> insertBatchWordDefinitionMap(
@@ -62,11 +62,17 @@ public class BatchInsertionDefinitionService {
 
                 // Realizamos la inserción en la base de datos
                 if (!definitionModelSet.isEmpty()) {
-                    wordDefinitionRepository.saveAll(definitionModelSet);
+                    List<WordDefinitionModel>  insertedDefinitionsIntoDatabase =  wordDefinitionRepository.saveAll(definitionModelSet);
+                    Set<DefinitionEstructurePojo> setDefinicionesPOJO = buildDefinitionEstructurePojos(insertedDefinitionsIntoDatabase, stringWordListDefinitionsPojoMap);
+
+                    // Llenar el mapa de salida con las definiciones insertadas
+                    for (DefinitionEstructurePojo pojo : setDefinicionesPOJO) {
+                        insertedWordDefinitionsMap.put(pojo.getWordDefinitionModel().getWord().getWord(), pojo);
+                    }
+
+                    //Insertar batch de ejemplos de las palabras
+                    batchesInsertionExamplesService.insertBatchExamplesList(setDefinicionesPOJO);
                 }
-
-                //Insertar batch de ejemplos de las palabras
-
 
             } catch (Exception e) {
                 log.error("Error processing definitions batch: {}", e.getMessage(), e);
@@ -75,4 +81,43 @@ public class BatchInsertionDefinitionService {
 
         return insertedWordDefinitionsMap;
     }
+
+
+    /**
+     * Devuelve una lista que contiene un POJO con la información de: definicion palabra + Set<String> ejemplos
+     * @param insertedDefinitions
+     * @param stringWordListDefinitionsPojoMap
+     * @return
+     */
+    private Set<DefinitionEstructurePojo> buildDefinitionEstructurePojos(
+            List<WordDefinitionModel> insertedDefinitions,
+            Map<String, WordListDefinitionsPojo> stringWordListDefinitionsPojoMap) {
+
+        Set<DefinitionEstructurePojo> definitionEstructurePojos = new HashSet<>();
+
+        for (WordDefinitionModel definitionModel : insertedDefinitions) {
+            String word = definitionModel.getWord().getWord(); //De la definición insertada se obtiene la palabra
+
+            WordListDefinitionsPojo wordListPojo = stringWordListDefinitionsPojoMap.get(word);
+            if (wordListPojo != null) {
+                // Buscar los ejemplos asociados a esta definición específica
+                List<String> examples = wordListPojo.getWordDefinitionDTOSet().stream()
+                        .filter(dto -> dto.getDefinition().equals(definitionModel.getWordDefinition()))
+                        .findFirst()
+                        .map(dto -> new ArrayList<>(dto.getExamples()))
+                        .orElse(new ArrayList<>());
+
+                //Genera el builder con el DefinitionStructurePojo
+                DefinitionEstructurePojo pojo = DefinitionEstructurePojo.builder()
+                        .wordDefinitionModel(definitionModel)
+                        .listExamples(examples)
+                        .build();
+
+                definitionEstructurePojos.add(pojo);
+            }
+        }
+
+        return definitionEstructurePojos;
+    }
+
 }
---
+++ API/src/main/java/com/martingago/words/service/batchInsertion/BatchesInsertionExamplesService.java
@@ -0,0 +1,60 @@
+package com.martingago.words.service.batchInsertion;
+
+import com.martingago.words.POJO.DefinitionEstructurePojo;
+import com.martingago.words.model.WordExampleModel;
+import com.martingago.words.repository.WordExampleRepository;
+import com.martingago.words.utils.BatchUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+public class BatchesInsertionExamplesService {
+
+    @Autowired
+    WordExampleRepository wordExampleRepository;
+
+    /**
+     * Sobre un listado de
+     * @param setDefinicionesPojo
+     * @return
+     */
+    public Set<WordExampleModel> insertBatchExamplesList(Set<DefinitionEstructurePojo> setDefinicionesPojo) {
+        // Declaración del set a devolver con los ejemplos insertados:
+        Set<WordExampleModel> insertedExamples = new HashSet<>();
+
+        BatchUtils.processInBatches(setDefinicionesPojo, 50, batch -> {
+            try {
+                Set<WordExampleModel> examplesToInsert = new HashSet<>();
+
+                // Recorre la lista de definiciones existentes:
+                for (DefinitionEstructurePojo definitionStructure : batch) {
+                    // De cada definición, extrae el set de ejemplos que puede contener
+                    Set<WordExampleModel> examples = definitionStructure.getListExamples().stream()
+                            .map(exampleModel -> WordExampleModel.builder()
+                                    .example(exampleModel)
+                                    .wordDefinitionModel(definitionStructure.getWordDefinitionModel())
+                                    .build())
+                            .collect(Collectors.toSet());
+
+                    examplesToInsert.addAll(examples); // Añade el set de ejemplos por definición
+                }
+
+                // Realiza la inserción por batches:
+                if (!examplesToInsert.isEmpty()) {
+                    insertedExamples.addAll(wordExampleRepository.saveAll(examplesToInsert)); // Acumula en lugar de sobrescribir
+                }
+
+            } catch (Exception e) {
+                log.error("Error processing example batch: {}", e.getMessage(), e);
+            }
+        });
+
+        return insertedExamples;
+    }
+}
