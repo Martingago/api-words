5e4fb7aabf8b41df214ef3e8d14aeb6c66d2d199
Martin <gagochorenmartin@gmail.com>
2025-03-03 19:10:47 +0100

    +updated menu app

--- frontend/wordradar/src/app/components/global/menu/menu.component.ts
+++ frontend/wordradar/src/app/components/global/menu/menu.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
-import { LinkComponent } from '../link/link.component';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  imports: [LinkComponent],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, CommonModule],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  menuOpen = false; // Estado del menú (abierto/cerrado)
+  isMobile = false; // Indica si la pantalla es móvil o tablet
 
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    // Detecta el tamaño de la pantalla (móvil y tablet)
+    this.breakpointObserver.observe([Breakpoints.XSmall, Breakpoints.Small])
+      .subscribe(result => {
+        console.log(result); // Muestra el resultado de la detección
+        this.isMobile = result.matches; // Actualiza el estado de isMobile
+        if (!this.isMobile) {
+          this.menuOpen = true; // Siempre muestra el menú en escritorio
+        } else {
+          this.menuOpen = false; // Oculta el menú en móvil y tablet por defecto
+        }
+      });
+  }
+
+  // Función para alternar el menú
+  toggleMenu() {
+    console.log("toggle menu. Mobile: " + this.isMobile);
+    if (this.isMobile) {
+      this.menuOpen = !this.menuOpen;
+    }
+  }
 }
---
+++ frontend/wordradar/tailwind.config.ts
@@ -0,0 +1,18 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#EB3933',
+        },
+      },
+      screens: {
+        'xs': '0px',
+        'sm': '600px',
+        'md': '960px',
+        'lg': '1280px',
+        'xl': '1920px',
+      }
+    },
+    // otras configuraciones
+  };
