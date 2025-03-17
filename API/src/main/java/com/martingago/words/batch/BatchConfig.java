e3f3512a99ca9753434680264063c68ba217cde8
Martin <gagochorenmartin@gmail.com>
2025-03-18 00:20:58 +0100

    batch changes

--- API/src/main/java/com/martingago/words/batch/BatchConfig.java
+++ API/src/main/java/com/martingago/words/batch/BatchConfig.java
@@ -1,30 +1,76 @@
 package com.martingago.words.batch;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.mapping.JsonLineMapper;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
 
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.mapping.JsonLineMapper;
 import org.springframework.batch.item.json.JacksonJsonObjectReader;
 import org.springframework.batch.item.json.JsonItemReader;
 import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @RequiredArgsConstructor
 public class BatchConfig {
 
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+    private final EntityManagerFactory entityManagerFactory;
+
+
     @Bean
-    public JsonItemReader<WordBatch> jsonLineItemReader() {
-        return new JsonItemReaderBuilder<WordBatch>()
-                .jsonObjectReader(new JacksonJsonObjectReader<>(WordBatch.class))
-                .resource(new ClassPathResource("data.jsonl"))
-                .name("wordBatchJsonItemReader")
-                .build();
+    public FlatFileItemReader<WordBatch> jsonLineItemReader() {
+        FlatFileItemReader<WordBatch> reader = new FlatFileItemReader<>();
+        reader.setResource(new ClassPathResource("files/test.jsonl"));
+        reader.setName("wordBatchJsonItemReader");
+
+        // Configurar el LineMapper para JSONL
+        DefaultLineMapper<WordBatch> lineMapper = new DefaultLineMapper<>();
+        JsonLineMapper jsonLineMapper = new JsonLineMapper();
+        jsonLineMapper.setObjectMapper(new ObjectMapper());
+        lineMapper.setLineTokenizer(line -> line); // Cada línea es un objeto JSON completo
+        lineMapper.setFieldSetMapper(jsonLineMapper); // Mapear la línea a WordBatch
+
+        reader.setLineMapper(lineMapper);
+
+        return reader;
     }
 
 
+    @Bean
+    public JpaItemWriter<WordBatch> wordWriter() {
+        return new JpaItemWriterBuilder<WordBatch>()
+                .entityManagerFactory(entityManagerFactory)
+                .build();
+    }
+
+    @Bean
+    public Step wordBatchStep() {
+        return new StepBuilder("wordBatchStep", jobRepository)
+                .<WordBatch, WordBatch>chunk(10, transactionManager)
+                .reader(jsonLineItemReader())
+                .writer(wordWriter())
+                .build();
+    }
+
+    @Bean
+    public Job runJob() {
+        return new JobBuilder("wordJob", jobRepository)
+                .start(wordBatchStep())
+                .build();
+    }
 }
--- API/src/main/java/com/martingago/words/batch/WordUploadController.java
+++ API/src/main/java/com/martingago/words/batch/WordUploadController.java
@@ -3,13 +3,17 @@ package com.martingago.words.batch;
 import com.martingago.words.dto.global.ApiResponseDTO;
 import com.martingago.words.dto.word.response.WordResponseViewDTO;
 import com.martingago.words.utils.JsonValidation;
+import feign.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -18,35 +22,52 @@ import java.util.Set;
 
 @RestController
 @RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class WordUploadController {
 
-    @Autowired
-    JsonValidation jsonValidation;
+    private final Job job;
+    private final JobLauncher jobLauncher;
 
-    @PostMapping("/upload-jsonl")
-    public ResponseEntity<ApiResponseDTO<String>> uploadJsonlFile(@RequestParam("file") MultipartFile file) {
-            if(jsonValidation.isValidJsonFile(file)){
-                try{
-                    Set<WordResponseViewDTO> wordSet = jsonValidation.parseJsonlFileToWordSet(file);
-                    wordSet.forEach(System.out::println);
-                    return ApiResponseDTO.build(
-                            true,
-                            "file upload",
-                            HttpStatus.OK.value(),
-                            "exito",
-                            HttpStatus.OK);
-                }catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }else{
-                return ApiResponseDTO.build(
-                        false,
-                        "Error uploading file",
-                        HttpStatus.BAD_REQUEST.value(),
-                        "Fail",
-                        HttpStatus.BAD_REQUEST
-                );
-            }
+    @GetMapping("/validate-jsonl")
+    public void validateJsonlFile(){
+        try {
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addLong("time", System.currentTimeMillis()) // Asegura una ejecución única
+                    .toJobParameters();
+
+            JobExecution execution = jobLauncher.run(job, jobParameters);
+            System.out.println("Estado del Job: " + execution.getStatus());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
+
+//    @PostMapping("/upload-jsonl")
+//    public ResponseEntity<ApiResponseDTO<String>> uploadJsonlFile(@RequestParam("file") MultipartFile file) {
+//            if(jsonValidation.isValidJsonFile(file)){
+//                try{
+//                    Set<WordResponseViewDTO> wordSet = jsonValidation.parseJsonlFileToWordSet(file);
+//                    wordSet.forEach(System.out::println);
+//                    return ApiResponseDTO.build(
+//                            true,
+//                            "file upload",
+//                            HttpStatus.OK.value(),
+//                            "exito",
+//                            HttpStatus.OK);
+//                }catch (IOException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }else{
+//                return ApiResponseDTO.build(
+//                        false,
+//                        "Error uploading file",
+//                        HttpStatus.BAD_REQUEST.value(),
+//                        "Fail",
+//                        HttpStatus.BAD_REQUEST
+//                );
+//            }
+//
+//    }
 }
