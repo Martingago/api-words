cfa564d010a268db9aa0ab49f8d298fe83c133fb
Martin <gagochorenmartin@gmail.com>
2025-04-11 13:52:09 +0200

    +completed refactor global functions to process entities DTO

--- API/src/main/java/com/martingago/words/batch/word/procesor/WordBatchProcessor.java
+++ API/src/main/java/com/martingago/words/batch/word/procesor/WordBatchProcessor.java
@@ -1,17 +1,9 @@
 package com.martingago.words.batch.word.procesor;
 
-import com.martingago.words.context.DefinitionProcessedContext;
 import com.martingago.words.domain.model.*;
-import com.martingago.words.domain.service.definition.CreateDefinitionService;
-import com.martingago.words.domain.service.example.CreateExampleService;
 import com.martingago.words.domain.service.word.CreateWordModelService;
-import com.martingago.words.domain.service.relation.CreateWordRelationsService;
 import com.martingago.words.dto.word.request.WordBatchDTO;
 import com.martingago.words.dto.word.request.WordBatchReferenceDTO;
-import com.martingago.words.dto.WordDefinitionDTO;
-import com.martingago.words.domain.repository.WordQualificationRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -30,22 +22,12 @@ import java.util.*;
 public class WordBatchProcessor implements ItemProcessor<WordBatchDTO, WordModel>, ItemStream {
 
     private final CreateWordModelService createWordModelService;
-    private final CreateDefinitionService createDefinitionService;
 
-    //Memoria local para almacenar los idiomas existentes
-    private Map<String, LanguageModel> languageMap = new HashMap<>();
 
-    //Memoria local para almacenar las qualificaciones existentes
-    private Map<String, WordQualificationModel> qualificationMap = new HashMap<>();
-
-    //Palabras ya existentes en la base de datos
-    private Map<String, WordBatchReferenceDTO> chunkWordMap = new HashMap<>();
-
-    //Palabras que se van a persistir en la base de datos
-    private Map<String, WordModel> newWordBatchMap = new HashMap<>();
-
-    @PersistenceContext
-    private EntityManager entityManager;
+    private Map<String, LanguageModel> languageMap = new HashMap<>(); //Memoria local idiomas existentes.
+    private Map<String, WordQualificationModel> qualificationMap = new HashMap<>(); //Memoria local qualifications existentes
+    private Map<String, WordModel> newWordsModelToPersist = new HashMap<>(); //Memoria local palabras relacionadas a persistir
+    private Map<String, WordBatchReferenceDTO> existingDBWordsMap = new HashMap<>(); //Memoria local palabras existentes
 
     private ExecutionContext executionContext;
 
@@ -56,53 +38,20 @@ public class WordBatchProcessor implements ItemProcessor<WordBatchDTO, WordModel
 
     @Override
     public void update(ExecutionContext executionContext) throws ItemStreamException {
-        newWordBatchMap.clear();
+        newWordsModelToPersist.clear();
     }
 
     @Override
     public WordModel process(WordBatchDTO item) throws Exception {
         // Recuperamos el mapa previamente almacenado en el ExecutionContext
-        chunkWordMap = (Map<String, WordBatchReferenceDTO>) this.executionContext.get("wordBatchMap");
-        newWordBatchMap = (Map<String, WordModel>) this.executionContext.get("newWordsToPersistMap");
+        existingDBWordsMap = (Map<String, WordBatchReferenceDTO>) this.executionContext.get("wordBatchMap");
+        newWordsModelToPersist = (Map<String, WordModel>) this.executionContext.get("newWordsToPersistMap");
 
-        WordModel wordModel;
-
-        // PRIMERA COMPROBACIÓN: Verificar si la palabra ya está en el mapa temporal de palabras nuevas
-        wordModel = newWordBatchMap.get(item.getWord());
-        if (wordModel != null) {
-            // La palabra ya se ha creado en este lote como placeholder
-            // Actualizamos para que ya no sea placeholder y añadimos datos completos
-            wordModel.setPlaceholder(false);
-            createDefinitionService.processDefinitions(item, wordModel, qualificationMap, newWordBatchMap, chunkWordMap);
-            return wordModel;
-        }
-
-        // SEGUNDA COMPROBACIÓN: Verificar si existe en la base de datos
-        WordBatchReferenceDTO existingWordBatch = chunkWordMap != null ? chunkWordMap.get(item.getWord()) : null;
-        if (existingWordBatch != null) {
-            // Si la palabra ya existe, comprobamos si no es un placeholder
-            if (!existingWordBatch.isPlaceholder()) {
-                return null; // Ya existe como palabra completa, no placeholder
-            }
-            // Si existe y es un placeholder, trabajamos sobre ese objeto
-            wordModel = entityManager.getReference(WordModel.class, existingWordBatch.getId());
-            wordModel.setPlaceholder(false);
-        } else {
-            // Si no se encuentra en ninguno de los mapas, se crea un nuevo objeto
-            LanguageModel language = languageMap.get(item.getLanguage());
-            if(language != null){
-                wordModel = createWordModelService.createWord(item,language);
-            }
-            if (wordModel != null) {
-                // Almacenar en el mapa temporal de palabras nuevas (no persistidas)
-                newWordBatchMap.put(item.getWord(), wordModel);
-            }
-        }
-
-        if (wordModel != null) {
-            createDefinitionService.processDefinitions(item, wordModel, qualificationMap, newWordBatchMap, chunkWordMap);
-        }
-        return wordModel;
+        //Se llama a la función encargada de procesar los datos de una palabra en las diferentes entidades que la componen.
+        return createWordModelService.insertWordIntoDatabase(item,
+                languageMap,
+                qualificationMap,
+                newWordsModelToPersist,
+                existingDBWordsMap);
     }
-
 }
--- API/src/main/java/com/martingago/words/domain/service/word/CreateWordModelService.java
+++ API/src/main/java/com/martingago/words/domain/service/word/CreateWordModelService.java
@@ -1,14 +1,80 @@
 package com.martingago.words.domain.service.word;
 
 import com.martingago.words.domain.model.*;
+import com.martingago.words.domain.service.definition.CreateDefinitionService;
 import com.martingago.words.dto.word.request.WordBatchDTO;
+import com.martingago.words.dto.word.request.WordBatchReferenceDTO;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @RequiredArgsConstructor
 @Service
 public class CreateWordModelService {
+
+    private final CreateDefinitionService createDefinitionService;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    /**
+     * Añade una palabra con sus definiciones a la base de datos
+     * @param wordBatchDTO objeto con la información que se quiere persistir.
+     * @param languageMap map con los idiomas existentes en la base de datos.
+     * @param qualificationMap map con la información de las qualifications existentes en la base de datos.
+     * @param newWordsModelToPersist map con la información de las palabras que se van a persistir en la base de datos.
+     * @param existingDBWordsMap map con la información de las palabras que ya existen en la base de datos.
+     * @return WordModel que se va a persistir en la base de datos.
+     */
+    public WordModel insertWordIntoDatabase(WordBatchDTO wordBatchDTO,
+                                            Map<String, LanguageModel> languageMap,
+                                            Map<String, WordQualificationModel> qualificationMap,
+                                            Map<String, WordModel> newWordsModelToPersist,
+                                            Map<String, WordBatchReferenceDTO> existingDBWordsMap
+                                            ){
+        WordModel wordModel;
+
+        // PRIMERA COMPROBACIÓN: Verificar si la palabra ya está en el mapa temporal de palabras nuevas
+        wordModel = newWordsModelToPersist.get(wordBatchDTO.getWord());
+        if (wordModel != null) {
+            // La palabra ya se ha creado en este lote como placeholder
+            // Actualizamos para que ya no sea placeholder y añadimos datos completos
+            wordModel.setPlaceholder(false);
+            createDefinitionService.processDefinitions(wordBatchDTO, wordModel, qualificationMap, newWordsModelToPersist, existingDBWordsMap);
+            return wordModel;
+        }
+
+        // SEGUNDA COMPROBACIÓN: Verificar si existe en la base de datos
+        WordBatchReferenceDTO existingWordBatch = existingDBWordsMap != null ? existingDBWordsMap.get(wordBatchDTO.getWord()) : null;
+        if (existingWordBatch != null) {
+            // Si la palabra ya existe, comprobamos si no es un placeholder
+            if (!existingWordBatch.isPlaceholder()) {
+                return null; // Ya existe como palabra completa, no placeholder
+            }
+            // Si existe y es un placeholder, trabajamos sobre ese objeto
+            wordModel = entityManager.getReference(WordModel.class, existingWordBatch.getId());
+            wordModel.setPlaceholder(false);
+        } else {
+            // Si no se encuentra en ninguno de los mapas, se crea un nuevo objeto
+            LanguageModel language = languageMap.get(wordBatchDTO.getLanguage());
+            if(language != null){
+                wordModel = createWord(wordBatchDTO,language);
+            }
+            if (wordModel != null) {
+                // Almacenar en el mapa temporal de palabras nuevas (no persistidas)
+                newWordsModelToPersist.put(wordBatchDTO.getWord(), wordModel);
+            }
+        }
+
+        if (wordModel != null) {
+            createDefinitionService.processDefinitions(wordBatchDTO, wordModel, qualificationMap, newWordsModelToPersist, existingDBWordsMap);
+        }
+        return wordModel;
+    }
 
     /**
      * Crea la entidad que se va a persistir en la base de datos de WordModel
